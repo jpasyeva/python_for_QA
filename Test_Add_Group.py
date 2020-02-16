75c15e980c053a0ff2af66ba723f5917321ffb58
Julia <jp@utair.digital>
2020-02-16 19:04:54 +0300

    Передача параметров в методы. Добавление второго тестового сценария.

--- Test_Add_Group.py
+++ Test_Add_Group.py
@@ -12,30 +12,29 @@ class TestAddGroup(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/index.php")
 
-    def login(self, wd):
-        # login
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name_group, header, footer):
         # создание новой группы
         wd.find_element_by_name("new").click()
         # заполнение формы
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("New")
+        wd.find_element_by_name("group_name").send_keys(name_group)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("New group")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("comment")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # нажатие кнопки создать
         wd.find_element_by_name("submit").click()
 
@@ -48,9 +47,18 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name_group="New", header="New group", footer="comment")
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.create_group(wd, name_group="", header="", footer="")
         self.return_to_group_page(wd)
         self.logout(wd)
 
