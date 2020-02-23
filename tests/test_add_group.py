d619dff64dcfc623fc7db905d224ca42c2743b8c
Julia <jp@utair.digital>
2020-02-23 15:38:35 +0300

    Созданы классы-помощники и перенесены вспомогательные функции в эти классы. Создан класс-менеджер, который инициализирует всех помощников.

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name_group="New", header="New group", footer="comment"))
+    app.group.create(Group(name_group="New", header="New group", footer="comment"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name_group="", header="", footer=""))
+    app.group.create(Group(name_group="", header="", footer=""))
     app.session.logout()
--- tests/test_create_new_contact.py
+++ tests/test_create_new_contact.py
@@ -11,7 +11,7 @@ def app(request):
 
 def test_create_new_contact(app):
     app.session.login("admin", "secret")
-    app.add_new_contact(Contact(firstname=u"Фамилия", middlename=u"Отчество", lastname=u"Имя", nickname=u"Никнейм", title =u"Заголовок", company=u"Компания", address= u"Адрес",
-                             home_phone=u"Домашний телефон", mobile_phone=u"Мобильный телефон", work_phone=u"Рабочий телефон", fax=u"Факс",
-                             email=u"электронная почта", homepage=u"сайт", address2=u"адрес2", phone2=u"дом", notes=u"заметка"))
+    app.contact.add_new(Contact(firstname=u"Фамилия", middlename=u"Отчество", lastname=u"Имя", nickname=u"Никнейм", title =u"Заголовок", company=u"Компания", address=u"Адрес",
+                                home_phone=u"Домашний телефон", mobile_phone=u"Мобильный телефон", work_phone=u"Рабочий телефон", fax=u"Факс",
+                                email=u"электронная почта", homepage=u"сайт", address2=u"адрес2", phone2=u"дом", notes=u"заметка"))
     app.session.logout()
