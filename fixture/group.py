11a1099267012803a23e8e9e2955f91ded17de1e
Julia <jp@utair.digital>
2020-02-27 21:36:16 +0300

    Выделен отдельный метод для заполнения формы группы. В методах создания и редактирования группы используется выделенный метод

--- fixture/group.py
+++ fixture/group.py
@@ -9,11 +9,8 @@ class GrouptHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def filling_form(self, group):
         wd = self.app.wd
-        self.open_group_page()
-        # создание новой группы
-        wd.find_element_by_name("new").click()
         # заполнение формы
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").click()
@@ -23,6 +20,14 @@ class GrouptHelper:
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # создание новой группы
+        wd.find_element_by_name("new").click()
+        # вызов метода заполнения формы
+        self.filling_form(group)
         # нажатие кнопки создать
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
@@ -43,15 +48,8 @@ class GrouptHelper:
         wd.find_element_by_name("selected[]").click()
         # нажать кнопку edit
         wd.find_element_by_name("edit").click()
-        # изменить форму группы
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name_group)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # вызов метода заполнения формы
+        self.filling_form(group)
         #нажать кнопку update
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
