ec4b7730b551db4b7705ff87f0c5cafe8291c689
Julia <jp@utair.digital>
2020-03-01 13:29:19 +0300

    Добавлена валидация фикстуры перед каждым тестом. Тесты выполяются в одном браузере под одной сессией

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,12 +3,7 @@ from models.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name_group="New", header="New group", footer="comment"))
-    app.session.logout()
-
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name_group="", header="", footer=""))
-    app.session.logout()
--- tests/test_create_new_contact.py
+++ tests/test_create_new_contact.py
@@ -3,16 +3,13 @@ from models.contact import Contact
 
 
 def test_create_new_contact(app):
-    app.session.login("admin", "secret")
     app.contact.add_new(Contact(firstname=u"Фамилия", middlename=u"Отчество", lastname=u"Имя", nickname=u"Никнейм", title =u"Заголовок", company=u"Компания", address=u"Адрес",
                                 home_phone=u"Домашний телефон", mobile_phone=u"Мобильный телефон", work_phone=u"Рабочий телефон", fax=u"Факс",
                                 email=u"электронная почта", homepage=u"сайт", address2=u"адрес2", phone2=u"дом", notes=u"заметка"))
-    app.session.logout()
+
 
 def test_create_empty_contact(app):
-    app.session.login("admin", "secret")
     app.contact.add_new(
         Contact(firstname=u"", middlename=u"", lastname=u"", nickname=u"", title=u"",
                 company=u"", address=u"", home_phone=u"", mobile_phone=u"", work_phone=u"",
                 fax=u"", email=u"", homepage=u"", address2=u"", phone2=u"", notes=u""))
-    app.session.logout()
