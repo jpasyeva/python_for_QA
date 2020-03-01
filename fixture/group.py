439ba8aafedbcfb8ac7e991b240fa3c33a689a24
Julia <jp@utair.digital>
2020-03-01 23:29:00 +0300

    Оптимизированы переходы между страницами в группах

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GrouptHelper:
 
     def open_group_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def fill_form_group(self, group):
         wd = self.app.wd
