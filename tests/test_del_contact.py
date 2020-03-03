fd55153c4103b0b3a5dbaeb3dc8a794463c6db3f
Julia <jp@utair.digital>
2020-03-03 21:42:38 +0300

    Изменила проверку присутсвия элементов на странице контактов

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -3,12 +3,12 @@ from models.contact import Contact
 
 
 def test_delete_first_contact(app):
-    if app.contact.count_select_checkbox() == 0:
+    if app.contact.count_edit() == 0:
         app.contact.add_new(Contact(firstname=u"First contact"))
     app.contact.delete_first_contact()
 
 
 def test_delete_all_contacts(app):
-    if app.contact.count_select_checkbox() == 0:
+    if app.contact.count_edit() == 0:
         app.contact.add_new(Contact(firstname=u"First contact"))
     app.contact.delete_all_contacts()
