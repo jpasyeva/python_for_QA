d6f11be42beb290336ebc5d25ea44f3c27aa0580
Julia <jp@utair.digital>
2020-03-12 13:56:57 +0300

    Доработан тест редактирования контактов

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -8,13 +8,13 @@ def test_edit_first_contact(app):
         app.contact.add_new(Contact(firstname="First contact"))
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
-    contact = Contact(firstname="autotest")
+    contact = Contact(firstname="autotest", lastname="autotest")
     contact.id = old_contacts[index].id
     app.contact.edit_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count_edit()
     new_contacts = app.contact.get_contact_list()
     old_contacts[index] = contact
-    assert old_contacts == new_contacts
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 # def test_edit_contact_lastname(app):
