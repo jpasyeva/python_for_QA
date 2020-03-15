c443e937ca22ca7746dc137eb8032d08669606b3
Julia <jp@utair.digital>
2020-03-15 19:47:33 +0300

    Добавлена случайность в тесты

--- tests/test_phones.py
+++ tests/test_phones.py
@@ -5,8 +5,10 @@ import re
 
 
 def test_contact_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact = app.contact.get_contact_list()
+    index = randrange(len(contact))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
@@ -15,8 +17,10 @@ def test_contact_on_home_page(app):
 
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact = app.contact.get_contact_list()
+    index = randrange(len(contact))
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
     assert contact_from_view_page.mobile_phone == contact_from_edit_page.mobile_phone
     assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
