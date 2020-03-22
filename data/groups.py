a31eb4e49fffacaed79eb97fac842be0b5efb493
Julia <jp@utair.digital>
2020-03-22 17:30:26 +0300

    динамическая генерация тестов

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+from models.group import Group
+
+
+testdata = [
+    Group(name_group="Name1", header="Header1", footer="Footer1"),
+    Group(name_group="Name2", header="Header2", footer="Footer2")
+]
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
 from models.group import Group
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name_group="", header="", footer="")] + [
-    Group(name_group=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
