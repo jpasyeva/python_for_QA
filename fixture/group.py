519917d60b48a99a526a361564a75c5c7c2b73f9
Julia <jp@utair.digital>
2020-03-29 20:33:38 +0300

    В тесте списки объектов загружаются из БД Реализованы новые метода для удаления групп по id

--- fixture/group.py
+++ fixture/group.py
@@ -43,7 +43,14 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         self.select_group_by_index(index)
-        # нажать кнопку удаления
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+        self.group_cache = None
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
         self.group_cache = None
@@ -67,6 +74,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def return_to_group_page(self):
         wd = self.app.wd
