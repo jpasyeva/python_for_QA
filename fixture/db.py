bd4e4f876cf92f278f673787317ffb8f916ee754
Julia <jp@utair.digital>
2020-03-29 20:11:16 +0300

    небольшие правки

--- fixture/db.py
+++ fixture/db.py
@@ -9,7 +9,7 @@ class DbFixture:
         self.database = database
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=database, user=user, password=password)
+        self.connection = pymysql.connect(host=host, database=database, user=user, password=password, autocommit=True)
 
     def get_group_list(self):
         list = []
--- tests/test_db_matches_ui.py
+++ tests/test_db_matches_ui.py
@@ -6,6 +6,7 @@ def test_group_list_compare_ui_and_db(app, db):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name_group="Test", header="Test", footer="Test"))
     ui_group = app.group.get_group_list()
+
     def clean (group):
         return Group(id_group=group.id_group, name_group=group.name_group.strip())
     db_groups = map(clean, db.get_group_list())
@@ -15,8 +16,9 @@ def test_group_list_compare_ui_and_db(app, db):
 def test_contacts_list_compare_ui_and_db(app, db):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Test", lastname="TEST"))
+    ui_contact = app.contact.get_contact_list()
+
     def clean(contact):
         return Contact(id_contact=contact.id_contact, lastname=contact.lastname.strip())
     db_contact = map(clean, db.get_contact_list())
-    ui_contact = app.contact.get_contact_list()
     assert sorted(ui_contact, key=Contact.id_or_max) == sorted(db_contact, key=Contact.id_or_max)
