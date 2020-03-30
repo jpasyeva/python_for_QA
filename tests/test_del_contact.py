8c4b2c56f0a3bdac6ae6540eed2d2c2dbd5c12d1
Julia <jp@utair.digital>
2020-03-30 19:42:39 +0300

    отключаемые проверки пользовательского интерфейса в тестах контактов

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -3,7 +3,7 @@ from models.contact import Contact
 import random
 
 
-def test_delete_some_contact(app, db):
+def test_delete_some_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.add_new(Contact(firstname=u"First contact"))
     old_contacts = db.get_contact_list()
@@ -12,6 +12,8 @@ def test_delete_some_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_group_list(), key=Contact.id_or_max)
 
 
 def test_delete_all_contacts(app, db):
--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -3,7 +3,7 @@ from models.contact import Contact
 from random import randrange
 
 
-def test_edit_some_contact(app, db):
+def test_edit_some_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.add_new(Contact(firstname="First contact"))
     old_contacts = db.get_contact_list()
@@ -20,6 +20,8 @@ def test_edit_some_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_group_list(), key=Contact.id_or_max)
 
 
 # def test_edit_contact_lastname(app):
