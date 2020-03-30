09f2c2a2cb5adcd49da6f9bd3b9f10b7e789f926
Julia <jp@utair.digital>
2020-03-30 19:38:52 +0300

    отключаемые проверки пользовательского интерфейса в тестах групп

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -3,7 +3,7 @@ from models.group import Group
 from random import randrange
 
 
-def test_edit_group_name(app, db):
+def test_edit_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name_group="First group"))
     old_groups = db.get_group_list()
@@ -15,3 +15,5 @@ def test_edit_group_name(app, db):
     new_groups = db.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
